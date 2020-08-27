72714adb6a029ef258a7813b101db038787a145f
kabrams <kabrams@bleacherreport.com>
2020-08-26 23:57:22 -0400

    10th commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,6 +12,7 @@ public class Test {
     
     public static void main(String[] args){
         System.out.println("Hello World! This is only the beginning! Get ready!");
+        System.out.println("Welcome!");
     }
     
 }
