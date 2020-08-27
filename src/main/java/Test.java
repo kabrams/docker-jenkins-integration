a3f1fbed4f06142edb1139cc5ff1e331c1e65514
kabrams <kabrams@bleacherreport.com>
2020-08-26 23:11:21 -0400

    6th commit, made modifications

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -11,7 +11,7 @@
 public class Test {
     
     public static void main(String[] args){
-        System.out.println("Hello World! This is only the beginning!");
+        System.out.println("Hello World! This is only the beginning! Get ready!");
     }
     
 }
