41ad3ee5b009c64aff45994a5cba6622ff31cc46
kabrams <kabrams@bleacherreport.com>
2020-08-27 22:38:32 -0400

    12th - modified test java file

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -13,6 +13,7 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello World! This is only the beginning! Get ready!");
         System.out.println("Welcome!");
+        System.out.println("testing out new jenkins config");
     }
     
 }
